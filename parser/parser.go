dcb775ee5c2fcdd1c37587f972543f54614d641a
Vefot <benjamin200@hotmail.co.uk>
2023-08-28 23:47:53 +0100

    parser error catching

--- parser/parser.go
+++ parser/parser.go
@@ -4,6 +4,7 @@ import (
 	"farcical/ast"
 	"farcical/lexer"
 	"farcical/token"
+	"fmt"
 )
 
 type Parser struct {
@@ -11,16 +12,26 @@ type Parser struct {
 
 	curToken  token.Token
 	peekToken token.Token
+	errors    []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{l: l, errors: []string{}}
 
 	// Read 2 tokens so curToken and peekToken are both set
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("Expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken() {
@@ -85,6 +96,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
 
