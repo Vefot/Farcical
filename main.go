152fef720df1a0e77561040eec72f42c3f30ce0e
Vefot <benjamin200@hotmail.co.uk>
2023-09-08 18:26:23 +0100

    can now run files with main

--- main.go
+++ main.go
@@ -1,13 +1,37 @@
 package main
 
 import (
+	"farcical/evaluator"
+	"farcical/lexer"
+	"farcical/parser"
 	"farcical/repl"
+	"flag"
 	"fmt"
+	"io"
 	"os"
 	"os/user"
 )
 
 func main() {
+	filepath := flag.String("file", "", "Path to file to interpret")
+	flag.Parse()
+
+	if *filepath != "" {
+		code, err := os.ReadFile(*filepath)
+		if err != nil {
+			fmt.Printf("Error reading file: %v\n", err)
+			return
+		}
+		codeString := string(code)
+		l := lexer.New(codeString)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		evaluated := evaluator.Eval(program)
+		io.WriteString(os.Stdout, evaluated.Inspect())
+		io.WriteString(os.Stdout, "\n")
+		return
+	}
+
 	user, err := user.Current()
 	if err != nil {
 		panic(err)
