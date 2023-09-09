f695d107904a1264fadeed17a0e997bedf65fcec
Vefot <benjamin200@hotmail.co.uk>
2023-09-10 00:09:43 +0100

    add string object and evaluation

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -59,6 +59,8 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		env.Set(node.Name.Value, val) // create the variable in the environment
 	case *ast.Identifier:
 		return evalIdentifier(node, env)
+	case *ast.StringLiteral:
+		return &object.String{Value: node.Value}
 	case *ast.FunctionLiteral:
 		params := node.Parameters
 		body := node.Body
