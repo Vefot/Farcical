50134c2314d55b781d31ae80e5ecf77e73178538
Vefot <benjamin200@hotmail.co.uk>
2023-09-12 21:41:06 +0100

    print function

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "farcical/object"
+import (
+	"farcical/object"
+	"fmt"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": &object.Builtin{
@@ -88,6 +91,14 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"print": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Print(arg.Inspect() + " ")
+			}
+			return &object.String{Value: ""}
 		},
 	},
 }
